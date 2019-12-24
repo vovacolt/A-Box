7c30e580a62d22c932fba84d9c7265ac658255c3
NechushkinGit <123>
2019-12-24 08:44:35 +0300

    now profile works

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,4 +2,8 @@ class ProjectsController < ApplicationController
   def index
     render json: {message: 'Привет', projects: Project.all}
   end
+
+  def project_menu
+    @projects = Project.All
+  end
 end
